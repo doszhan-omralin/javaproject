0845bccefdd0b4af0ce497f79f0331c6461507ee
doszhan <ffacool5@gmail.com>
2023-04-30 15:27:28 +0600

    asdas

--- Java/src/doszhan/Main.java
+++ Java/src/doszhan/Main.java
@@ -1,11 +1,9 @@
 package doszhan;
-
+import java.util.Scanner;
 public class Main {
-
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		
-		System.out.println("МЫ ЛУЧШИЕ ПРОГРАММИСТЫ");
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		System.out.println("s");
 	}
-
 }
